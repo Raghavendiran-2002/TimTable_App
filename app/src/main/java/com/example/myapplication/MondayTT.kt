4409efb288200dd3102bb9c3dc2603e00ec47e06
root <raghavendiran46461@gmail.com>
2022-02-01 10:59:16 +0530

    Inital Commit 2

--- app/src/main/java/com/example/myapplication/MondayTT.kt
+++ app/src/main/java/com/example/myapplication/MondayTT.kt
@@ -3,13 +3,22 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MondayTT : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monday_tt2)
+
+        var intent = intent
+        var restext = intent.getStringExtra("Name")
+        var hr_11 = intent.getStringExtra("Hour")
+        var min_11 = intent.getStringExtra("Min")
+        var sub = findViewById<TextView>(R.id.Subject1)
+        sub.text = restext  + "\t" + hr_11 + "\t" + min_11
         val addTT = findViewById<FloatingActionButton>(R.id.AddTT)
         addTT.setOnClickListener{
             val intent = Intent(this, MondayTTupdate::class.java)
--- app/src/main/java/com/example/myapplication/MondayTTupdate.kt
+++ app/src/main/java/com/example/myapplication/MondayTTupdate.kt
@@ -1,11 +1,37 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MondayTTupdate : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monday_ttupdate)
+
+        val hr_1 = findViewById<TextView>(R.id.Monday_hour)
+        val min_1 = findViewById<TextView>(R.id.Monday_minutes)
+        val editText = findViewById<TextView>(R.id.Mon_Subject)
+        val updateBut = findViewById<Button>(R.id.updateButton)
+
+        updateBut.setOnClickListener {
+            val subjectName = editText.toString()
+            val hr1 = hr_1.toString()
+            var min1 = min_1.toString()
+            val intent = Intent(this, MondayTT::class.java)
+            intent.putExtra("Name", subjectName)
+            intent.putExtra("Hour", hr1)
+            intent.putExtra("Min", min1)
+            startActivity(intent)
+
+        }
     }
 }
+
+
+
